e855437494ce52f0ecc8fc502ec7d8d32cbb5637
Luan Kenzley <kenzleydev@outlook.com>
2023-05-19 21:16:18 -0300

    Implementando anotações JPA para classes de dominio

--- src/main/java/br/com/luankenzley/helpdesk/api/domain/Chamado.java
+++ src/main/java/br/com/luankenzley/helpdesk/api/domain/Chamado.java
@@ -2,14 +2,22 @@ package br.com.luankenzley.helpdesk.api.domain;
 
 import br.com.luankenzley.helpdesk.api.domain.enums.Prioridade;
 import br.com.luankenzley.helpdesk.api.domain.enums.Status;
+import com.fasterxml.jackson.annotation.JsonFormat;
 
+import javax.persistence.*;
+import java.io.Serializable;
 import java.time.LocalDate;
 import java.util.Objects;
+@Entity
+public class Chamado implements Serializable {
 
-public class Chamado {
-
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
+
+    @JsonFormat(pattern = "dd/MM/yyyy")
     private LocalDate dataAbertura = LocalDate.now();
+    @JsonFormat(pattern = "dd/MM/yyyy")
     private LocalDate dataFechamento;
     private Prioridade prioridade;
     private Status status;
@@ -17,7 +25,11 @@ public class Chamado {
     private String observacoes;
 
 
+    @ManyToOne
+    @JoinColumn(name = "tecnico_id")
     private Tecnico tecnico;
+    @ManyToOne
+    @JoinColumn(name = "cliente_id")
     private Cliente cliente;
 
     public Chamado() {
--- src/main/java/br/com/luankenzley/helpdesk/api/domain/Tecnico.java
+++ src/main/java/br/com/luankenzley/helpdesk/api/domain/Tecnico.java
@@ -1,24 +1,28 @@
 package br.com.luankenzley.helpdesk.api.domain;
 
+import br.com.luankenzley.helpdesk.api.domain.enums.Perfil;
+
+import javax.persistence.Entity;
+import javax.persistence.OneToMany;
 import java.util.ArrayList;
 import java.util.List;
-
+@Entity
 public class Tecnico extends Pessoa {
 
+    @OneToMany(mappedBy = "tecnico")
     private List<Chamado> chamados = new ArrayList<>();
 
     public Tecnico() {
         super();
+        addPerfis(Perfil.TECNICO);
     }
-
     public Tecnico(Integer id, String nome, String cpf, String email, String senha) {
         super(id, nome, cpf, email, senha);
+        addPerfis(Perfil.TECNICO);
     }
-
     public List<Chamado> getChamados() {
         return chamados;
     }
-
     public void setChamados(List<Chamado> chamados) {
         this.chamados = chamados;
     }
