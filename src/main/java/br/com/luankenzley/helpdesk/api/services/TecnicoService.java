0422914818a9acdac33ac467ff25193f18a64a63
Luan Kenzley <kenzleydev@outlook.com>
2023-05-20 21:29:56 -0300

    implementando endpoint findAll para técnico

--- src/main/java/br/com/luankenzley/helpdesk/api/services/TecnicoService.java
+++ src/main/java/br/com/luankenzley/helpdesk/api/services/TecnicoService.java
@@ -6,6 +6,7 @@ import br.com.luankenzley.helpdesk.api.services.exceptions.ObjectNotFoundExcepti
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -17,5 +18,9 @@ public class TecnicoService {
     public Tecnico findById(Integer id) {
         Optional<Tecnico> tecnicoById = tecnicoRepository.findById(id);
         return tecnicoById.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado! id: " + id));
+    }
+
+    public List<Tecnico> findAll() {
+        return tecnicoRepository.findAll();
     }
 }
